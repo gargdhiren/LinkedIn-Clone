0aac84477afdcc3a4fd7525a28a79eee9556c39f
Dhiren Garg <1234dhirengarg@gmail.com>
2026-04-27 16:52:57 +0530

    files were not added commit again

--- connections-service/src/main/java/com/linkedin/connections_service/repository/PersonsRepository.java
+++ connections-service/src/main/java/com/linkedin/connections_service/repository/PersonsRepository.java
@@ -1,4 +1,15 @@
 package com.linkedin.connections_service.repository;
 
-public class PersonsRepository {
+import com.linkedin.connections_service.entity.Persons;
+import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.neo4j.repository.query.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PersonsRepository extends Neo4jRepository<Persons, Long> {
+    Optional<Persons> getByName(String name);
+
+    @Query("MATCH (personA:Persons) -[:CONNECTED_TO]- (personB:Persons) WHERE personA.userId = $userId RETURN personB")
+    List<Persons> getFirstDegreeConnections(Long userId);
 }
--- connections-service/src/main/java/com/linkedin/connections_service/service/ConnectionsService.java
+++ connections-service/src/main/java/com/linkedin/connections_service/service/ConnectionsService.java
@@ -1,4 +1,23 @@
 package com.linkedin.connections_service.service;
 
+
+import com.linkedin.connections_service.entity.Persons;
+import com.linkedin.connections_service.repository.PersonsRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+@Slf4j
 public class ConnectionsService {
+    private final PersonsRepository personsRepository;
+
+    public List<Persons> getFirstDegreeConnections(Long userId){
+        log.info("Getting first degree connections for user {}",userId);
+
+        return personsRepository.getFirstDegreeConnections(userId);
+    }
 }
